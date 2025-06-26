fe076de918f2b41faa3d86b53da032014e26e118
studentKim2000 <166790301+studentKim2000@users.noreply.github.com>
2025-06-26 11:15:40 +0900

    유머튜브 웹서버에 올림

---
+++ humortube/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  const apple = 1
+    
+  return ( 
+  <div>
+    <h1>유머튜브</h1>
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/RsSvjr2ocRw?si=3PpQTZBIlFngZ4Dq&amp;start=60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+
+    <h1>사진관련</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/PhHv-WQDcIw?si=oMBfGZyOm6yJwaH3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>   
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,8 @@
+
+let a = 10
+const b = 20
+const c = a + b
+
+a = 50
+
+console.log(c)
